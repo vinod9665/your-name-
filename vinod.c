684cbbff93c993dc904834963cf1ea9f040f3d81
vinod chahure <vinodchahure@gmail.com>
2025-09-15 05:51:45 -0400

    Add files via upload

---
+++ vinod.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a= 23, b= 22;
+    int c= a+b;
+    printf("addition is %d", c);
+    return 0;
+}
---
+++ vinod4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    float a= 23.10, b= 22.98;
+    float c= a+b;
+    printf("addition is %.2f", c);
+    return 0;
+}
